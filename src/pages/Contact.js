26d72f8a2f5fd02a2256ecd3fafd851a760a0de7
Patton Parham <patton.parham@verizon.net>
2020-04-26 07:00:38 -0500

    styling updates

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,41 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import "./Contact.css";
+import {Col, Row, Container} from "../components/Grid/Grid";
 const Contact = () => {
 
     return(
-        <div>
-            
+        <div className="contactcard">
+            <form action="mailto:patton.parham@verizon.net" method="post" enctype="text/plain" className="form">
+                <label for="exampleFormControlInput1">Name:</label>
+                <br></br>
+                <input type="text" name="name" className="form-control E"></input>
+                <label for="exampleFormControlInput1">Email address:</label>
+                <br></br>
+                <input type="text" name="mail" className="form-control E"></input>
+                <label for="exampleFormControlTextarea1">Message:</label>
+                <br></br>
+                <textarea type="text" name="comment" className="form-control" id="exampleFormControlTextarea1"></textarea>
+                <br></br>
+                <Row>
+                <button type="submit" value="Send" className="btn submitbtn" id = "mobilecontactbtn">
+                    <div className="btnborder">
+                        <div className="btntext">
+                            Submit
+                        </div>
+                    </div>
+                </button>
+                <br></br>
+                <button type="reset" value="Reset" className= "btn submitbtn" id ="resetbtn">  
+                            <div className= "btnborder">
+                                <div className= "btntext">
+                            Reset
+                            </div>
+                        </div> 
+                            
+                        </button>
+                </Row>
+            </form>
 
         </div>
     );
