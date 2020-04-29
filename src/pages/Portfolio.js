23f701d9bb31d420070c0a70bf9a4fe8fbe88901
Patton Parham <patton.parham@verizon.net>
2020-04-29 03:04:04 -0500

    huge spacing error with portfolio link labels fixed

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,7 +7,7 @@ const Portfolio = () => {
 
     return(
         <div>
-            <div className="portfoliocardtop">
+            <div className="portfoliocardtop nonmobilecard">
                 <Row className="topRow">
                     <Col size="sm">
                     <a href = "https://serene-ravine-67154.herokuapp.com/" className="employeeDirectoryLink">Employee Directory</a>
@@ -19,10 +19,18 @@ const Portfolio = () => {
                 <div className="middle">
                 <Row className="middleRowLink">
                     
-                <Container> 
-                <a href = "https://immense-hamlet-86412.herokuapp.com/" className="InnerJoinLink">InnerJoin</a>
-                <a href="https://github.com/PattonParham/Employee-Tracker" className="employeeTrackerLink">EmployeeTracker</a>
-                </Container>  
+              
+                    <Col size="sm-6">
+                    
+                <a href = "https://immense-hamlet-86412.herokuapp.com/" className="InnerJoinLink"> <button className="placeholder">Inner-Join</button></a>
+                
+                </Col>
+                <Col size="sm-6">
+                
+                <a href="https://github.com/PattonParham/Employee-Tracker" className="employeeTrackerLink"><button className="placeholder">Employee Tracker</button></a>
+                
+                </Col>
+                
                
                 </Row>
 
@@ -44,11 +52,12 @@ const Portfolio = () => {
                 <Row>
                     
                    <Container>
-                <a href = "https://jackjewell.github.io/TakeAHike//" className="TakeAHikeLink">TakeAHike</a>
-            
-                <a href = "https://pattonparham.github.io/Weather-Dashboard/" className="WeatherLink">WeatherDashboard</a>
-              
-                <a href = "https://pattonparham.github.io/Password-Generator/" className="PassGenLink">PasswordGenerator</a>
+                    
+                <a href = "https://jackjewell.github.io/TakeAHike//" className="TakeAHikeLink"><button className="placeholder">Take A Hike</button></a>
+                <a href = "https://pattonparham.github.io/Weather-Dashboard/" className="WeatherLink"><button className="placeholder">Weather Dashboard</button></a>
+                <a href = "https://pattonparham.github.io/Password-Generator/" className=""><button className="placeholder PassGenLink">Password Generator</button></a>
+                
+
                 </Container>
                 </Row>
                 </Col>
@@ -78,9 +87,91 @@ const Portfolio = () => {
             </div>
 
             <Row>
-                    <button  className="buttonRow linkedin"><a href="https://www.linkedin.com/in/patton-parham-016b851a5/" className="buttontext">LinkedIn</a></button>
-                    <button  className="buttonRow github"><a href="https://github.com/PattonParham" className="buttontext">GitHub</a></button>
-                    <button  className="buttonRow resume"><a href = "https://peaceful-river-21360.herokuapp.com/2020PattonJParhamResume.pdf" className="buttontext">Resume</a></button>
+                    <button  className="buttonRow linkedin nonmobilecard"><a href="https://www.linkedin.com/in/patton-parham-016b851a5/" className="buttontext">LinkedIn</a></button>
+                    <button  className="buttonRow github nonmobilecard" ><a href="https://github.com/PattonParham" className="buttontext">GitHub</a></button>
+                    <button  className="buttonRow resume nonmobilecard"><a href = "https://peaceful-river-21360.herokuapp.com/2020PattonJParhamResume.pdf" className="buttontext">Resume</a></button>
+            </Row>
+
+
+
+            <div className="portfoliocardtop mobilecard">
+                <Row className="topRow">
+                    <Col size="sm">
+                    <a href = "https://serene-ravine-67154.herokuapp.com/" className="employeeDirectoryLink">Employee Directory</a>
+                <div className="employeeDirectory gif"></div>
+                
+                </Col>
+
+                </Row>
+                <div className="middle">
+                <Row className="middleRowLink">
+                    
+                <Col size="sm-6">
+                    
+                    <a href = "https://immense-hamlet-86412.herokuapp.com/" className="InnerJoinLink"> <button className="placeholder">Inner-Join</button></a>
+                    
+                    </Col>
+                    <Col size="sm-6">
+                    
+                    <a href="https://github.com/PattonParham/Employee-Tracker" className="employeeTrackerLink"><button className="placeholder">Employee Tracker</button></a>
+                    
+                    </Col>  
+               
+                </Row>
+
+                <Row className="middleRow">
+                    <Col size="sm">
+                    
+                <div className="InnerJoin middlegif"></div>
+               
+                </Col>
+                <Col size="sm">
+                
+                <div className="employeeTracker middlegif"></div>
+                
+                </Col>
+
+                </Row>
+                <div className="Links">
+                    <Col size="lg">
+                <Row>
+                    
+                   <Container>
+                   <a href = "https://jackjewell.github.io/TakeAHike//" className="TakeAHikeLink"><button className="placeholder">Take A Hike</button></a>
+                <a href = "https://pattonparham.github.io/Weather-Dashboard/" className="WeatherLink"><button className="placeholder">Weather Dashboard</button></a>
+                <a href = "https://pattonparham.github.io/Password-Generator/" className=""><button className="placeholder PassGenLink">Password Generator</button></a>
+                </Container>
+                </Row>
+                </Col>
+                </div>
+                <Row className="bottomRow">
+                    <Col size="sm">
+                    
+                <div className="TakeAHike bottomgif"></div>
+                
+                </Col>
+                <Col size="sm">
+                
+                <div className="Weather bottomgif"></div>
+                
+                </Col>
+                <Col size="sm">
+                
+                <div className="PassGen bottomgif"></div>
+                
+                </Col>
+                </Row>
+                
+                </div>
+                
+
+
+            </div>
+
+            <Row className="mobilecard">
+                    <button  className="buttonRow linkedin mobilecard"><a href="https://www.linkedin.com/in/patton-parham-016b851a5/" className="buttontext">LinkedIn</a></button>
+                    <button  className="buttonRow github mobilecard"><a href="https://github.com/PattonParham" className="buttontext">GitHub</a></button>
+                    <button  className="buttonRow resume mobilecard"><a href = "https://peaceful-river-21360.herokuapp.com/2020PattonJParhamResume.pdf" className="buttontext">Resume</a></button>
             </Row>
         </div>
     );
